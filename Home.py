e4aae71ada8ed0b9777365244ca9aaffff22b34c
John Tian <johntian2014@gmail.com>
2022-07-30 19:38:12 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,7 +11,7 @@ st.sidebar.success("Select a tool above.")
 
 st.markdown(
     """
-    This app visualizes player or team xG statistics. All data is from understat.com. Perform searches on players using their understat ID. Perform searches on teams using the dropdown list. 
+    This app visualizes player or team xG statistics (Premier League, Bundesliga, La Liga, Serie A, Ligue 1). All data is from understat.com. Perform searches on players using their understat ID. Perform searches on teams using the dropdown list. 
 
     **👈 Select a tool from the sidebar** to get started!
     ### Features 
